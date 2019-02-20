8a762c22a0e0215791b21648b4e529d4fd85a25a
mr-fool <mr-fool@users.noreply.github.com>
2019-02-20 00:37:53 -0700

    testing

---
+++ compile.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+//console.log(fs.readFileSync("contracts/inbox.sol") );
+var input = {
+	language: 'Solidity',
+	sources: {
+		'inbox.sol': {
+			content:  fs.readFileSync("contracts/inbox.sol",'utf-8')
+		}
+	},
+	settings: {
+		outputSelection: {
+			'*': {
+				'*': [ '*' ]
+			}
+		}
+	}
+}
+
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+//console.log(output);
+// `output` here contains the JSON output as specified in the documentation
+/*for (var contractName in output.contracts['inbox.sol']) {
+	console.log(contractName + ': ' + output.contracts['inbox.sol'][contractName].evm.bytecode.object)
+}*/
+
+//console.log(output.contracts);
+
+module.exports = output.contracts["inbox.sol"].Inbox;
---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+//Don't put your real metamask address there
+const provider = new HDWalletProvider (
+    'hunt hint toy gift weekend leisure glimpse taxi program base drink supreme',
+    'https://rinkeby.infura.io/v3/727c635298344b37961bb1755114f08b'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(abi)
+    .deploy({data: '0x' + evm.bytecode.object , arguments: ['Hi there!']}) // add 0x bytecode
+    .send({from: accounts[0], gas: '2500000'}); // remove 'gas'
+
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
+
+//process.exit();
